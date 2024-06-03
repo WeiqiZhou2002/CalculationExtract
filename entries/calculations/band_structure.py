01c81e593be04642d5c531cbd31e486f90a25908
WeiqiZhou2002 <weiqizhou2002@gmail.com>
2024-06-03 16:55:23 +0800

    start structure part 3 in progress

--- entries/calculations/band_structure.py
+++ entries/calculations/band_structure.py
@@ -8,9 +8,212 @@
 @Date       : 2024/5/30 17:35 
 @Description: 
 """
+from public.tools.periodic_table import PTable
 from .base_calculation import BaseCalculation
+from public.lattice import Lattice
+from public.structure import Structure
+from .base_calculation import BaseCalculation
+import numpy as np
 
 
 class BandStructure(BaseCalculation):
     def __init__(self, file_parsers: dict):
+        self.atomicCharge = None
+        self.atomicMagnetization = None
+        self.linearMagneticMoment = None
         super().__init__(file_parsers)
+
+    def getThermoDynamicProperties(self):
+        child = self.vasprunParser.root.find("./calculation[last()]/energy/i[@name='e_fr_energy']")
+        totalenergy = float(child.text)
+        child = self.vasprunParser.root.find("./calculation[last()]/dos/i[@name='efermi']")
+        numberofatoms = int(self.vasprunParser.root.find("./atominfo/atoms").text)
+        fermienergy = float(child.text)
+        energyPerAtom = totalenergy / numberofatoms
+        formation_energy = 0.0
+        composition = self.vasprunParser.composition
+        energy_atoms = 0.
+        for comp in composition:
+            if comp.atomic_symbol in PTable().atom_energy:
+                energy_atoms += PTable().atom_energy[comp.atomic_symbol] * comp.amount
+            else:
+                energy_atoms = 0.
+                break
+        if energy_atoms != 0.:
+            formation_energy = (totalenergy - energy_atoms) / self.vasprunParser.numberOfSites
+
+        doc = {
+            'TotalEnergy': totalenergy,
+            'FermiEnergy': fermienergy,
+            'EnergyPerAtom': energyPerAtom,
+            'FormationEnergy': formation_energy
+        }
+        return doc
+
+    def getGapFromBand(self):
+        """
+                    能带能隙
+                    :return
+                """
+        eigenval = self.vasprunParser.eigenValues
+        if eigenval is None:
+            eigenval = self.getEigenValues()
+        isSpin = eigenval["IsSpinPolarized"]
+        number = eigenval["NumberOfGeneratedKPoints"]
+        BandsNum = eigenval["NumberOfBand"]
+        Energies = eigenval["EigenvalData"]
+        Kpoints = eigenval["KPoints"]
+        efermi = eigenval["FermiEnergy"]
+        energies = []
+        kpoints = []
+        for t in Energies.values():
+            energies.append(t)
+            kpoints.append(Kpoints)  # 存储两份kpoints，用于自旋情况计算能隙标记
+        energies = np.array(energies)
+        # print(energies)
+        energies = energies - efermi
+        # print(kpoints)
+
+        if isSpin:
+            index00 = []
+            index10 = []
+            index01 = []
+            index11 = []
+            vbmE0 = []  # cbm data
+            cbmE0 = []  # vbm data
+            vbmE1 = []  # cbm data
+            cbmE1 = []  # vbm data
+            kindex0 = -1
+            kindex1 = -1
+            for i in range(BandsNum):
+                if min(energies[0][i]) * max(energies[0][i]) < 0:
+                    return {"GapFromBand": "Metal"}
+                else:
+                    if min(energies[0][i]) < 0.0 or min(energies[0][i]) == 0.0:  # below fermi
+                        for j in range(number):
+                            if energies[0][i][j] == max(energies[0][i]):
+                                index00.append(j)
+                                vbmE0.append(energies[0][i][j])
+                                # print("the vbm band number is :", i, "kpints is :", kpoints[0][j], "energy is :",
+                                #       energies[0][i][j])
+                    else:  # above fermi
+                        for j in range(number):
+                            if energies[0][i][j] == min(energies[0][i]):
+                                index01.append(j)
+                                cbmE0.append(energies[0][i][j])
+                                # print("the cbm band number is :", i, "kpints is :", kpoints[0][j], "energy is :",
+                                #       energies[0][i][j])
+                    # print(index00, index01)
+
+                if min(energies[1][i]) * max(energies[1][i]) < 0:
+                    return {"GapFromBand": "Metal"}
+                else:
+                    if min(energies[1][i]) < 0.0 or min(energies[1][i]) == 0.0:  # below fermi
+                        for j in range(number):
+                            if energies[1][i][j] == max(energies[1][i]):
+                                index10.append(j)
+                                vbmE1.append(energies[1][i][j])
+                                # print("the vbm band number is :", i, "kpints is :", kpoints[1][j], "energy is :",
+                                #       energies[1][i][j])
+                    else:  # above fermi
+                        for j in range(number):
+                            if energies[1][i][j] == min(energies[1][i]):
+                                index11.append(j)
+                                cbmE1.append(energies[1][i][j])
+                                # print("the cbm band number is :", i, "kpints is :", kpoints[1][j], "energy is :",
+                                #       energies[1][i][j])
+
+            if min(cbmE0) < min(cbmE1):
+                cbmEnergy = min(cbmE0)
+                kindex1 = cbmE0.index(cbmEnergy)
+            else:
+                cbmEnergy = min(cbmE1)
+                kindex1 = cbmE1.index(cbmEnergy)
+            if max(vbmE0) < max(vbmE1):
+                vbmEnergy = max(vbmE1)
+                kindex0 = vbmE1.index(vbmEnergy)
+            else:
+                vbmEnergy = max(vbmE0)
+                kindex0 = vbmE0.index(vbmEnergy)
+
+            gapValue = cbmEnergy - vbmEnergy
+            if kindex0 == kindex1:
+                gaptype = "direct gap"
+            else:
+                gaptype = "indirect gap"
+            return {
+                "GapFromBand": gapValue,
+                "GapType": gaptype,
+                "VBMFromBand": vbmEnergy,
+                "CBMFromBand": cbmEnergy
+            }
+        else:
+            index00 = []
+            index01 = []
+            vbmE0 = []  # cbm data
+            cbmE0 = []  # vbm data
+            kindex0 = -1
+            kindex1 = -1
+            for i in range(BandsNum):
+                if min(energies[0][i]) * max(energies[0][i]) < 0:
+                    return {"GapFromBand": "Metal"}
+                else:
+                    if min(energies[0][i]) < 0.0 or min(energies[0][i]) == 0.0:  # below fermi
+                        for j in range(number):
+                            if energies[0][i][j] == max(energies[0][i]):
+                                index00.append(j)
+                                vbmE0.append(energies[0][i][j])
+                                # print("the vbm band number is :", i, "kpints is :", kpoints[0][j], "energy is :",
+                                #       energies[0][i][j])
+                    else:  # above fermi
+                        for j in range(number):
+                            if energies[0][i][j] == min(energies[0][i]):
+                                index01.append(j)
+                                cbmE0.append(energies[0][i][j])
+                                # print("the cbm band number is :", i, "kpints is :", kpoints[0][j], "energy is :",
+                                #       energies[0][i][j])
+                    # print(index00, index01)
+
+            cbmEnergy = min(cbmE0)
+            kindex1 = cbmE0.index(cbmEnergy)
+            vbmEnergy = max(vbmE0)
+            kindex0 = vbmE0.index(vbmEnergy)
+            gapValue = cbmEnergy - vbmEnergy
+            if kindex0 == kindex1:
+                gaptype = "direct gap"
+            else:
+                gaptype = "indirect gap"
+            return {
+                "GapFromBand": gapValue,
+                "GapType": gaptype,
+                "VBMFromBand": vbmEnergy,
+                "CBMFromBand": cbmEnergy
+            }
+
+    def to_bson(self):
+        doc = self.basicDoc
+        if self.outcarParser is not None:
+            self.atomicCharge, self.atomicMagnetization = self.outcarParser.getAtomicChargeAndAtomicMagnetization()
+        if self.oszicarParser is not None:
+            self.linearMagneticMoment = self.oszicarParser.getLinearMagneticMoment()
+        doc['ProcessData'] = {
+            'ElectronicSteps': self.getElectronicSteps()
+        }
+        doc['Properties'] = {
+            "ThermodynamicProperties": self.getThermoDynamicProperties(),
+            "ElectronicProperties": {
+                'AtomicCharge': self.atomicCharge,
+                'EigenValues': self.getEigenValues(),
+                'ProjectedEigenVal_on_IonOrbitals': vaspParser.projectedEigen,
+                'GapFromBand': self.getGapFromBand()
+            },
+            'MagneticProperties': {
+                'AtomicMagnetization': self.atomicMagnetization,
+                'LinearMagneticMoment': self.linearMagneticMoment,
+            }
+        }
+        doc['Files'] = [self.vasprunParser.vaspPath, self.file_parser['incar'].filepath,
+                        self.file_parser['outcar'].filepath, self.file_parser['kpoint'].filepath,
+                        self.file_parser['oszicar'].filepath]
+
+        return doc
